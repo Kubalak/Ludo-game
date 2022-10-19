c07b2bcaacb1ae2c642d3d53af8f5fb222f86fb8
Kubalak <kuba.jach@poczta.neostrada.pl>
2022-10-19 19:02:47 +0200

    Dodanie biblioteki JSON i test funkcjonalności.

---
+++ Check-chan/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+int main(int argc, char** argv, char** env) {
+	try {
+		std::ifstream f("sample.json");
+		nlohmann::json data = nlohmann::json::parse(f);
+		std::cout << data["quiz"]["sport"]["q1"]["question"] << '\n'; // Which one is correct team name in NBA?
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << '\n';
+		return 1;
+	}
+#ifdef _DEBUG 
+	system("pause"); // Stop konsole before program exits.
+#endif
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple ludo game project.
+
+# CHANGELOG <i>(all changes are shown below)</i>
+
+# v0.0.1
+- Created the project
+- Added <a href='https://github.com/nlohmann/json'>`JSON`</a> library
